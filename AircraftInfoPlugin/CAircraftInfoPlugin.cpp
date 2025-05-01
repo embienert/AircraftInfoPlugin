39e2c6e646b284dff574417b2c99554e6428e406
Martin Bienert <51266397+embienert@users.noreply.github.com>
2025-05-02 00:07:30 +0200

    Update version to 1.7

--- AircraftInfoPlugin/CAircraftInfoPlugin.cpp
+++ AircraftInfoPlugin/CAircraftInfoPlugin.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 
 const char* PLUGIN_NAME			= "Aircraft Info Plugin";
-const char* PLUGIN_VERSION		= "1.6";
+const char* PLUGIN_VERSION		= "1.7";
 const char* PLUGIN_AUTHOR		= "Martin Bienert";
 const char* PLUGIN_COPYRIGHT	= "Free to be distributed as source code";
 
